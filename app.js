76418b3148012fddc722486235191336e12bc65b
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2016-05-23 13:27:04 +0200

    Mejorado el ruteo de la app con sección oculta de admin e introducción a los controladores y base de datos en localStorage de html5

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ app.controller('TodoListController', function($scope) {
 });
 
 
+/*Ejemplo de uso de LocalStorage de html5 para simular la base de datos  de la app
+
+  var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+  // Put the object into storage
+  localStorage.setItem('testObject', JSON.stringify(testObject));
+
+  // Retrieve the object from storage
+  var retrievedObject = localStorage.getItem('testObject');
+
+  console.log('retrievedObject: ', JSON.parse(retrievedObject));
+*/
+
+
+
 //Más info sobre las rutas y sus posibilidades: https://github.com/angular-ui/ui-router
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -16,27 +31,36 @@ app.config(function($stateProvider, $urlRouterProvider) {
        //#Configuración de estados:
        $stateProvider
          //Configura la URL principal
-         .state('inicio',{
+
+         /*
+
+        */
+
+         .state('public',{
+           templateUrl:'publicTemplate.html',
+         })
+         //Vistas anidadas
+         .state('public.inicio',{
            url:'/inicio',
            templateUrl:'inicio.html',
          })
-         .state('reservas',{
+         .state('public.reservas',{
            url:'/reservas',
            templateUrl:'reservas.html',
          })
-         .state('ofertas',{
+         .state('public.ofertas',{
            url:'/ofertas',
            templateUrl:'ofertas.html',
          })
-         .state('ocio',{
+         .state('public.ocio',{
            url:'/ocio',
            templateUrl:'ocio.html',
          })
-         .state('servicios',{
+         .state('public.servicios',{
            url:'/servicios',
            templateUrl:'servicios.html',
          })
-         .state('acercade',{
+         .state('public.acercade',{
            url:'/acercade',
            templateUrl:'acercade.html',
          })
@@ -46,3 +70,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
          })
 
 });//Final de config. de las rutas.
+
+
+
+app.controller('loginController', function($scope){
+
+
+    $scope.login = function (credenciales) {
+      console.log('Logueando al usuario con credenciales:')
+      console.log(credenciales);
+
+      if (credenciales.username == 'admin' && credenciales.password=='admin'){
+        console.log('Usuario con permisos');
+      }
+
+    };
+
+});
